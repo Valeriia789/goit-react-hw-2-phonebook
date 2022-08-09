047376cb45e3a1dbcfa6f002d5c2d58fe2573241
Valeriia <valeriia.air@gmail.com>
2022-08-09 19:01:36 +0300

    рендер і видалення контакту

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,74 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework
-    </div>
-  );
-};
+import React, { Component } from 'react'
+import { nanoid } from 'nanoid'
+// model.id = nanoid()
+import initialContacts from './initialContacts.json'
+import { ContactsList } from './Contacts/ContactsList'
+
+class App extends Component {
+  // Стан оголошується в конструкторі, оскільки це перше, що відбувається,
+  //  коли створюється екземпляр класу.
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      contacts: initialContacts,
+      name: ''
+    }
+  }
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId)
+    }))
+  }
+  render () {
+    // console.log(Object.keys(this.state));
+    // console.log(Object.values(this.state));
+
+    return (
+      <>
+        <form>
+          <label>
+            Name
+            <input
+              type='text'
+              name='name'
+              placeholder='Entet Name'
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+
+          <button type='submit'>Add contact</button>
+        </form>
+
+        <ul>
+          <ContactsList
+            contacts={this.state.contacts}
+            onDeleteContact={this.deleteContact}
+          ></ContactsList>
+        </ul>
+      </>
+    )
+  }
+}
+
+export default App
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework
+//     </div>
+//   );
+// };
---
+++ src/components/Contacts/ContactsList.js
@@ -0,0 +1,13 @@
+export const ContactsList = ({ contacts, onDeleteContact }) => {
+  return (
+    <ul>
+      {contacts.map(contact => (
+        <li key={contact.id}>
+          <p> {contact.name} : </p>
+          <p> {contact.number} </p>
+          <button onClick={() => onDeleteContact(contact.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  )
+}
